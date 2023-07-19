8cd69fb33b67ed8ae273604ac18c8ce11ae55d1a
Keijiro Takahashi <keijiro@gmail.com>
2023-07-19 17:45:44 +0900

    Implement OOP version

---
+++ Assets/Scripts/SpawnBehavior.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Unity.Mathematics;
+using Random = Unity.Mathematics.Random;
+
+sealed class SpawnBehavior : MonoBehaviour
+{
+    [field:SerializeField] public GameObject Prefab = null;
+    [field:SerializeField] public int3 Dimensions = 10;
+    [field:SerializeField] public float Interval = 0.1f;
+    [field:SerializeField] public uint RandomSeed = 12345;
+
+    void Start()
+    {
+        var totalCount = Dimensions.x * Dimensions.y * Dimensions.z;
+
+        var rand = new Random(RandomSeed);
+        var offset = ((float3)Dimensions - 1) / 2;
+
+        for (var xi = 0; xi < Dimensions.x; xi++)
+            for (var yi = 0; yi < Dimensions.y; yi++)
+                for (var zi = 0; zi < Dimensions.z; zi++)
+                {
+                    var p = math.float3(xi, yi, zi);
+                    p += rand.NextFloat3(-0.1f, 0.1f) - offset;
+                    p *= Interval;
+
+                    var r = rand.NextQuaternionRotation();
+
+                    var go = Instantiate(Prefab, p, r);
+                }
+    }
+}
---
+++ Assets/Scripts/StirBehavior.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Unity.Mathematics;
+using Klak.Math;
+
+sealed class StirBehavior : MonoBehaviour
+{
+    [field:SerializeField] public float3 Angles = 10;
+    [field:SerializeField] public float Frequency = 1;
+    [field:SerializeField] public uint RandomSeed = 12345;
+
+    void FixedUpdate()
+    {
+        var elapsed = Time.time;
+
+        var freq = Frequency * math.float3(0.8f, 0.9f, 1.1f);
+        var time = freq * (elapsed + 1000);
+
+        var rot = Noise.Float3(time, RandomSeed);
+        rot *= math.radians(Angles);
+
+        GetComponent<Rigidbody>().MoveRotation(quaternion.Euler(rot));
+    }
+}
