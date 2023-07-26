3cc90de2bc1def62bdb914031aeb41340cd86b7d
Keijiro Takahashi <keijiro@gmail.com>
2023-07-26 16:59:03 +0900

    Cosmetic changes

--- Assets/Lottery/Scripts/RollbackSystem.cs
+++ Assets/Lottery/Scripts/RollbackSystem.cs
@@ -1,4 +1,3 @@
-using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Physics.Systems;
@@ -24,34 +23,28 @@ public partial class RollbackSystem : SystemBase
     {
         if (!_record.IsCreated) return;
 
-        var record = _record;
-        var i = 0;
-
+        var (record, i) = (_record, 0);
         Entities.ForEach( (ref LocalTransform xform,
                            ref PhysicsVelocity velocity) =>
-                          (xform, velocity) = record[i++]
-                        ).Schedule();
+                          (xform, velocity) = record[i++] ).Schedule();
     }
 
     void SaveState()
     {
+        if (_record.IsCreated) _record.Dispose();
+
         var count = 0;
         Entities.ForEach( (in LocalTransform xform,
                            in PhysicsVelocity velocity) =>
-                          count++
-                        ).Run();
-
-        if (_record.IsCreated) _record.Dispose();
+                          count++ ).Run();
 
         _record = new NativeArray<(LocalTransform, PhysicsVelocity)>
           (count, Allocator.Persistent);
-        var record = _record;
 
-        var i = 0;
+        var (record, i) = (_record, 0);
         Entities.ForEach( (in LocalTransform xform,
                            in PhysicsVelocity velocity) =>
-                          record[i++] = (xform, velocity)
-                        ).Schedule();
+                          record[i++] = (xform, velocity) ).Schedule();
     }
 
     protected override void OnUpdate()
@@ -61,66 +54,5 @@ public partial class RollbackSystem : SystemBase
         _request = (false, false);
     }
 }
-
-/*
-[UpdateInGroup(typeof(BeforePhysicsSystemGroup))]
-public partial struct RollbackSystem : ISystem
-{
-    NativeArray<(LocalTransform xform, PhysicsVelocity velocity)> _record;
-
-    public static void RequestSave() => _saveRequested = true;
-    public static void RequestLoad() => _loadRequested = true;
-
-    static bool _saveRequested;
-    static bool _loadRequested;
-
-    public void OnCreate(ref SystemState state)
-      => state.RequireForUpdate<Lottery>();
-
-    void LoadState(ref SystemState state)
-    {
-        if (!_record.IsCreated) return;
-
-        var i = 0;
-        foreach (var (xform, velocity) in
-                 SystemAPI.Query<RefRW<LocalTransform>,
-                                 RefRW<PhysicsVelocity>>())
-        {
-            xform.ValueRW = _record[i].xform;
-            velocity.ValueRW = _record[i].velocity;
-            i++;
-        }
-    }
-
-    void SaveState(ref SystemState state)
-    {
-        var count = 0;
-        foreach (var (xform, velocity) in
-                 SystemAPI.Query<RefRO<LocalTransform>,
-                                 RefRO<PhysicsVelocity>>()) count++;
-
-        if (_record.IsCreated) _record.Dispose();
-
-        _record = new NativeArray<(LocalTransform, PhysicsVelocity)>
-          (count, Allocator.Persistent);
-
-        var i = 0;
-        foreach (var (xform, velocity) in
-                 SystemAPI.Query<RefRO<LocalTransform>,
-                                 RefRO<PhysicsVelocity>>())
-        {
-            _record[i++] = (xform.ValueRO, velocity.ValueRO);
-        }
-    }
-
-    [BurstCompile]
-    public void OnUpdate(ref SystemState state)
-    {
-        if (_saveRequested) SaveState(ref state);
-        if (_loadRequested) LoadState(ref state);
-        _saveRequested = _loadRequested = false;
-    }
-}
-*/
 
 } // namespace EcsPhysicsTest.Lottery
